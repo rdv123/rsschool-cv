28f85ce166851292ae6373093e91458590661b37
Denis Rozhkov <rojkoff@gmail.com>
2022-01-10 00:56:29 +0300

    feat: get completed on codewars

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function init() {
   getCommits();
+  getCompleted();
 }
 init();
 
 async function getCommits() {
   let commits = document.querySelector(".li-commit");
   console.log("commit", commits.textContent);
+  let repositories = document.querySelector(".li-repos");
 
   console.log("test");
   const data = await fetch(`https://api.github.com/users/rdv123/repos`);
@@ -30,4 +32,18 @@ async function getCommits() {
   }, 0);
   console.log("count", count);
   commits.textContent = `Commits on GitHub - ${count}`;
+  repositories.textContent = `Repositories on GitHub - ${dataParse.length}`;
+}
+
+async function getCompleted() {
+  let completed = document.querySelector(".li-kata");
+  let liHonor = document.querySelector(".li-honor");
+  const dataKata = await fetch(`https://www.codewars.com/api/v1/users/rdv123/`);
+  let dataKataParse = await dataKata.json();
+  console.log("kata", dataKataParse.codeChallenges.totalCompleted);
+  console.log("honor", dataKataParse.honor);
+  let completedKata = dataKataParse.codeChallenges.totalCompleted;
+  let honor = dataKataParse.honor;
+  completed.textContent = `Completed katas on Codewars - ${completedKata}`;
+  liHonor.textContent = `Honor on Codewars - ${honor}`;
 }
